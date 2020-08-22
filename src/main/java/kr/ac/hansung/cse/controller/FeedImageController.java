15495f184d5fa8160b993a1d10fc171ebf17ef12
sangminkim <apfhdznzl@gmail.com>
2020-08-22 18:15:20 +0900

    피드이미지 전체조회 메소드 추가

--- src/main/java/kr/ac/hansung/cse/controller/FeedImageController.java
+++ src/main/java/kr/ac/hansung/cse/controller/FeedImageController.java
@@ -1,11 +1,13 @@
 package kr.ac.hansung.cse.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kr.ac.hansung.cse.model.FUser;
 import kr.ac.hansung.cse.model.FeedImage;
-import kr.ac.hansung.cse.model.FeedImageLike;
 import kr.ac.hansung.cse.repo.FUserRepository;
 import kr.ac.hansung.cse.repo.FeedImageRepository;
 
@@ -26,6 +27,28 @@ public class FeedImageController {
 	@Autowired
 	FeedImageRepository feedImageRepository;
 
+	@GetMapping("/{id}")
+	public ResponseEntity<List<FeedImage>> getAllImages(@PathVariable("id") String id) {
+		List<FeedImage> images = new ArrayList<>();
+		try {
+			feedImageRepository.findAll().forEach(images::add);
+			List<FeedImage> _images = new ArrayList<FeedImage>();
+			for(FeedImage image : images) {
+				if(image.getUserId().equals(id)) {
+					_images.add(image);
+				}
+			}
+
+			if (_images.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(_images, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<FUser> updateFeedImage(@PathVariable("id") String id, @RequestBody FeedImage image) {
 		Optional<FUser> userData = fUserrepository.findById(id);
