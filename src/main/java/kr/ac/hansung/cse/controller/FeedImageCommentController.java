cc10e8c05fabaa31b201c41d56e0cfad0b0f67f6
sangminkim <apfhdznzl@gmail.com>
2021-01-26 02:23:24 +0900

    이미지 댓글 삭제 메소드 생성

--- src/main/java/kr/ac/hansung/cse/controller/FeedImageCommentController.java
+++ src/main/java/kr/ac/hansung/cse/controller/FeedImageCommentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -58,4 +59,9 @@ public class FeedImageCommentController {
 		return new ResponseEntity<>(commentData, HttpStatus.OK);
 	}
 
+	@DeleteMapping("/{id}")
+	public ResponseEntity<HttpStatus> deleteImageComment(@PathVariable("id") Integer id) {
+		feedImageCommentRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
